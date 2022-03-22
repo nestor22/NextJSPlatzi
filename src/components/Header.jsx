67d599cce8e7a7defb7fe1fee06ab7172e92a1b8
ngonzalez <nestor.ge222@gmail.com>
2022-03-21 22:16:36 -0600

    actualizacion paths en jsconfig

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from "react";
 import "@styles/Header.scss";
 import Menu from "@components/Menu";
 
-import AppContext from "../context/AppContext";
+import AppContext from "@context/AppContext";
 import iconMenu from "@icons/icon_menu.svg";
 import logo from "@logos/logo_yard_sale.svg";
 import shoppingCard from "@icons/icon_shopping_cart.svg";
