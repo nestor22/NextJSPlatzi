19dedd629bd358d264585f5bdfc847c6af0cc1de
ngonzalez <nestor.ge222@gmail.com>
2022-03-30 22:40:13 -0600

    productos lista

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,39 +1,35 @@
-import React, { useState, useContext } from "react";
-import Image from "next/image"
-import AppContext from "@context/AppContext";
-import addToCartImage from "@icons/bt_add_to_cart.svg";
-import styles from "@styles/ProductItem.module.scss";
+import React, { useContext } from 'react';
+import AppContext from '@context/AppContext';
+import addToCartImage from '@icons/bt_add_to_cart.svg';
+import addedToCartImage from '@icons/bt_added_to_cart.svg';
+import styles from '@styles/ProductItem.module.scss';
 
-const ProductItem = ({product}) => {
- 
-  const { addToCart } = useContext(AppContext);
+const ProductItem = ({ product }) => {
+	const { state, addToCart } = useContext(AppContext);
 
-  const handleClick = item => {
-    addToCart(item);
-  };
+	const handleClick = item => {
+		console.log('in cart: ', state.cart.includes(item));
+		addToCart(item)
+	}
 
-  return (
-    <div className={styles.ProductItem}>
-      <Image
-        src={product.images[0]} 
-        alt={product.title}
-        width="100%"
-        height="100%"
-        layout="fill"
-      />
-      <div className={styles['product-info']}>
-        <div>
-          <p>${product.price}</p>
-          <p>{product.title}</p>
-        </div>
-        <figure onClick={()=>handleClick(product)}>
-          <Image src={addToCartImage} alt=""  width="100%"
-        height="100%"
-        layout="fill"/>
-        </figure>
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className={styles.ProductItem}>
+			<img src={product.images[0]} alt={product.title} />
+			<div className={styles['product-info']}>
+				<div>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
+				</div>
+				<figure className={styles['more-clickable-area']} onClick={() => handleClick(product)} >
+					{state.cart.includes(product) ? <img
+						className={styles.disabled, styles['add-to-cart-btn']}
+						src={addedToCartImage}
+						alt="added to cart"
+					/> : <img className={styles['add-to-cart-btn'], styles.pointer} src={addToCartImage} alt="add to cart" />}
+				</figure>
+			</div>
+		</div>
+	);
+}
 
 export default ProductItem;
