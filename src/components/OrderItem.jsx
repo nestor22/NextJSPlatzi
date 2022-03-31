853cba8c3dfdd6893dd837e06fdd375e61bb19d0
ngonzalez <nestor.ge222@gmail.com>
2022-03-30 22:52:49 -0600

    add checkout

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import iconClose from '@icons/icon_close.png';
 import styles from '@styles/OrderItem.module.scss';
 
@@ -6,10 +7,10 @@ const OrderItem = ({product}) => {
 	return (
 		<div className={styles.OrderItem}>
 			<figure>
-				<img src={product.images[0]} alt="bike" />
+			{product?.images[0] && <Image src={product.images[0]} alt={product.title} width={240} height={240} />}
 			</figure>
-			<p>{product.title}</p>
-			<p>${product.price}</p>
+			<p>{product?.title}</p>
+			<p>${product?.price}</p>
 			<img src={iconClose} alt="close" />
 		</div>
 	);
