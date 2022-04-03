78c86344b42c84eb3ed99b30b8c04abf8deb8867
ngonzalez <nestor.ge222@gmail.com>
2022-04-02 21:23:38 -0600

    update scripts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <title>React Shop</title>
           {/* solo para lo de google analitys 
                      <script dangerouslySetInnerHTML={{
                         __html: `sadf`
