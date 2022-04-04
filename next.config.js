1e301fa4a5e87200c6fe027041ba2f581b0b6d86
ngonzalez <nestor.ge222@gmail.com>
2022-04-03 19:05:47 -0600

    pwa configuracion archivos

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public', 
+    register: true,
+    mode: 'production', 
+    disabled: false
+  },
   reactStrictMode: true,
   images:{
     domains: ['placeimg.com', 'localhost', 'api.lorem.space']
   }
-};
-
-module.exports = nextConfig;
+})
